9a101ec5eab8901a0091a29525317b57b9c730fc
tomas-reves <tomas.reves@gmail.com>
2022-05-28 16:44:51 +0300

    names list

--- 14_flask/vardai_db_uzd/main.py
+++ 14_flask/vardai_db_uzd/main.py
@@ -1,41 +1,37 @@
-""""Sukurti programą, kuri leistų įvesti vardą ir jį išsaugotų duomenų bazėje. Įvesti vardai turėtų būti atspausdinti eilės tvarka su skaičiukais.
+""""Sukurti programą, kuri leistų įvesti vardą ir jį išsaugotų duomenų bazėje. Įvesti vardai turėtų būti atspausdinti eilės tvarka su skaičiukais."""
 
-Patarimas naudoti:
-<ol> ir <li>
-"""
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vardai.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Vardai(db.Model):
+class Name(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
-@app.route("/", methods=['GET', 'POST'])
-def vardai():
-    if request.method == "POST":
-        name_entry = request.form['name']
-        name = Vardai(name=name_entry)
+@app.route("/", methods=['POST', 'GET'])
+@app.route("/index", methods=['POST', 'GET'])
+def main():
+    if request.method == 'POST':
+        name_tag = request.form['name']
+        name = Name(name=name_tag)
         db.session.add(name)
         db.session.commit()
-
+        return redirect('/')
     else:
-        name_obj = Vardai.query.all()
-        print(name_obj)
-        return render_template('styles.html', name_obj=name_obj)
-
-
-
-
-
-    return render_template("style.html")
+        names = Name.query.all()
+        print(names)
+        return render_template('index.html', names=names)
 
 if __name__ == "__main__":
     app.run(debug=True)
