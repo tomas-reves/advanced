cfde47491c00c7db6a1d7ea9b399e3a13ba9c993
tomas-reves <tomas.reves@gmail.com>
2022-06-18 20:31:00 +0300

    rulete

---
+++ rulete/roulette_game.py
@@ -0,0 +1,94 @@
+from roulette_model import RouletteGame
+
+number_bet_input = None
+place_number_bet_amount = None
+color_bet_input = None
+place_color_bet_amount = None
+odds_evens_bet_input = None
+place_odds_evens_bet_amount = None
+dozens_bet_input = None
+place_dozens_bet_amount = None
+low_high_nums_bet_input = None
+place_low_high_nums_bet_amount = None
+columns_bet_input = None
+place_columns_bet_amount = None
+dict_of_bets = {}
+while True:
+    print("==========================================================")
+    game_input = int(input(f"ENTER NUMBER TO SELECT GAME\n 1 -- Guess roulette number\n 2 -- Guess color of the number\n "
+                           "3 -- Guess odds or evens\n 4 -- Guess dozens\n 5 -- Guess low or high numbers\n 6 -- Guess"
+                           "columns\n 7 -- Finish bet and play "))
+    print("==========================================================")
+    if game_input == 1:
+        while True:
+            number_of_num_bets = int(input("Enter how many numbers you want to place bet on: "))
+            for num in range(number_of_num_bets): # allows to place bets on more than one number
+                number_bet_input = int(input("Enter the number from 0 to 36 "))
+                place_number_bet_amount = int(input("Enter your bet for this game, the winning will be 2 to 1: "))
+                dict_of_bets[number_bet_input] = place_number_bet_amount
+            break
+
+    if game_input == 2:
+        while True:
+            color_bet_input = input("Enter color: red or black ")
+            place_color_bet_amount = int(input("Enter your bet for this game, the winning will be 2 to 1: "))
+            break
+    if game_input == 3:
+        while True:
+            odds_evens_bet_input = input("Enter odd or even ")
+            place_odds_evens_bet_amount = int(input("Enter your bet for this game, the winning will be 2 to 1: "))
+            break
+    if game_input == 4:
+        while True:
+            dozens_bet_input = input("Enter dozens: 1st 12, 2nd 12 or 3rd 12 ")
+            place_dozens_bet_amount = int(input("Enter your bet for this game, the winning will be 2 to 1: "))
+            break
+    if game_input == 5:
+        while True:
+            low_high_nums_bet_input = input("Low numbers (1-18), High numbers (19-36), enter: low or high ")
+            place_low_high_nums_bet_amount = int(input("Enter your bet for this game, the winning will be 2 to 1: "))
+            break
+    if game_input == 6:
+        while True:
+            columns_bet_input = input("Enter: 1st column, 2nd column or 3rd column")
+            place_columns_bet_amount = int(input("Enter your bet for this game, the winning will be 2 to 1: "))
+            break
+    if game_input == 7:
+        winnings = 0
+        roulette_game_obj = RouletteGame()
+
+        if number_bet_input != None:
+            for k, v in dict_of_bets.items():
+                roulette_game_obj.number_game(k, v)
+                if roulette_game_obj.random_roulette_selection == k:
+                    winnings += (v * 2)
+
+        if color_bet_input != None:
+            roulette_game_obj.color_game(color_bet_input, place_color_bet_amount)
+            if roulette_game_obj.check_number_color() == color_bet_input:
+                winnings += (place_color_bet_amount * 2)
+
+        if odds_evens_bet_input != None:
+            roulette_game_obj.even_odd_game(odds_evens_bet_input, place_odds_evens_bet_amount)
+            if roulette_game_obj.check_if_num_even_or_odd() == odds_evens_bet_input:
+                winnings += (place_odds_evens_bet_amount * 2)
+
+        if dozens_bet_input != None:
+            roulette_game_obj.dozen_game(dozens_bet_input, place_dozens_bet_amount)
+            if roulette_game_obj.check_number_dozen() == dozens_bet_input:
+                winnings += (place_dozens_bet_amount * 2)
+
+        if low_high_nums_bet_input != None:
+            roulette_game_obj.low_high_game(low_high_nums_bet_input, place_low_high_nums_bet_amount)
+            if roulette_game_obj.check_if_num_low_high() == low_high_nums_bet_input:
+                winnings += (place_low_high_nums_bet_amount * 2)
+
+        if columns_bet_input != None:
+            roulette_game_obj.columns_game(columns_bet_input, place_columns_bet_amount)
+            if roulette_game_obj.check_if_num_columns() == columns_bet_input:
+                winnings += (place_columns_bet_amount * 2)
+
+        print(f"Your winnings: {winnings}")
+        break
+
+
---
+++ rulete/roulette_model.py
@@ -0,0 +1,117 @@
+import random
+
+class RouletteGame:  # EUROPEAN ROULETTE
+    red_color_roulette_list = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+    black_color_roulette_list = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+    green_color_roulette_list = [0]
+    first_dozen_roulette_list = list(range(1, 13))
+    second_dozen_roulette_list = list(range(13, 25))
+    third_dozen_roulette_list = list(range(25, 36))
+    low_roulette_num_list = list(range(1, 19))  # first half from 1 to 18
+    high_roulette_num_list = list(range(19, 37))  # second half from 19 to 36
+    first_column_num_list = list(range(1, 37, 3))
+    second_column_num_list = list(range(2, 37, 3))
+    third_column_num_list = list(range(3, 37, 3))
+
+    random_roulette_selection = random.randint(0, 37)  # this is a random roulette number selection
+
+    def number_game(self, bet_number, bet_number_amount):
+        if bet_number == self.random_roulette_selection:
+            print(f"Roulette number - {self.random_roulette_selection}, your number: {bet_number} YOU WON {bet_number_amount * 2} EUR")
+        else:
+            print(f"Roulette number - {self.random_roulette_selection}, your number: {bet_number} YOU LOST {bet_number_amount} EUR")
+
+    def check_number_color(self):
+        if self.random_roulette_selection in self.red_color_roulette_list:
+            return "RED"
+        elif self.random_roulette_selection in self.black_color_roulette_list:
+            return "BLACK"
+        else:
+            return "GREEN"
+
+    def color_game(self, bet_color, bet_color_amount):
+        if self.check_number_color() == bet_color.upper():
+            print(f"You bet {bet_color}, roulette number: {self.random_roulette_selection}, which is "
+                  f"{self.check_number_color()}  - YOU WON {bet_color_amount * 2} EUR")
+        else:
+            print(f"You bet {bet_color}, roulette number: {self.random_roulette_selection}, which is "
+                  f"{self.check_number_color()}  - YOU LOST {bet_color_amount} EUR")
+
+    def check_if_num_even_or_odd(self):
+        if self.random_roulette_selection % 2 == 0:
+            return "EVEN"
+        else:
+            return "ODD"
+
+    def even_odd_game(self, bet_odd_or_even, bet_odd_or_even_amount):
+        if self.check_if_num_even_or_odd() == bet_odd_or_even.upper():
+            print(f"You bet {bet_odd_or_even}, roulette number: {self.random_roulette_selection}, which is "
+                  f"{self.check_if_num_even_or_odd()}  - YOU WON {bet_odd_or_even_amount * 2} EUR")
+        else:
+            print(f"You bet {bet_odd_or_even}, roulette number: {self.random_roulette_selection}, which is "
+                  f"{self.check_if_num_even_or_odd()}  - YOU LOST {bet_odd_or_even_amount} EUR")
+
+    def check_number_dozen(self):
+        if self.random_roulette_selection in self.first_dozen_roulette_list:
+            return "1st 12"
+        elif self.random_roulette_selection in self.second_dozen_roulette_list:
+            return "2nd 12"
+        elif self.random_roulette_selection in self.third_dozen_roulette_list:
+            return "3rd 12"
+        else:
+            return "not a part of of any dozens"
+
+    def dozen_game(self, bet_dozen, bet_dozen_amount):
+        if self.check_number_dozen() == bet_dozen:
+            print(f"You bet {bet_dozen}, roulette number: {self.random_roulette_selection}, which is "
+                  f"{self.check_number_dozen()}  - YOU WON {bet_dozen_amount * 2} EUR")
+        else:
+            print(f"You bet {bet_dozen}, roulette number: {self.random_roulette_selection}, which is "
+                  f"{self.check_number_dozen()}  - YOU LOST {bet_dozen_amount} EUR")
+
+    def check_if_num_low_high(self):
+        if self.random_roulette_selection in self.low_roulette_num_list:
+            return "LOW"
+        elif self.random_roulette_selection in self.high_roulette_num_list:
+            return "HIGH"
+        else:
+            return "is not a part of low or high number"
+
+
+    def low_high_game(self, bet_low_high, bet_low_high_amount):
+        if self.check_if_num_low_high() == bet_low_high.upper():
+            print(f"You bet {bet_low_high}, roulette number: {self.random_roulette_selection}, which is "
+                  f"{self.check_if_num_low_high()}  - YOU WON {bet_low_high_amount * 2} EUR")
+        else:
+            print(f"You bet {bet_low_high}, roulette number: {self.random_roulette_selection}, which is "
+                  f"{self.check_if_num_low_high()}  - YOU LOST {bet_low_high_amount} EUR")
+
+    def check_if_num_columns(self):
+        if self.random_roulette_selection in self.first_column_num_list:
+            return "1st column"
+        elif self.random_roulette_selection in self.second_column_num_list:
+            return "2nd column"
+        elif self.random_roulette_selection in self.second_column_num_list:
+            return "3rd column"
+        else:
+            return "is not a part of any columns"
+
+    def columns_game(self, bet_column, bet_column_amount):
+        if self.check_if_num_columns() == bet_column:
+            print(f"You bet {bet_column}, roulette number: {self.random_roulette_selection}, which is "
+                  f"{self.check_if_num_columns()}  - YOU WON {bet_column_amount * 2} EUR")
+        else:
+            print(f"You bet {bet_column}, roulette number: {self.random_roulette_selection}, which is "
+                  f"{self.check_if_num_columns()}  - YOU LOST {bet_column_amount} EUR")
+
+
+
+
+#gesas.number_check()
+
+# gesas.color_check("red")
+
+lygu = RouletteGame()
+#lygu.color_game("red", 20)
+lygu.low_high_game("high",100)
+# lygu.even_odd_game('even')
